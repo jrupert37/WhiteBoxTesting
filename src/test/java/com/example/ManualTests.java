8187ebf2cdc985fa3f8c8b254b7d09ebcbf9079c
Jackson Rupert <jrupert37@gatech.edu>
2024-11-10 16:08:18 -0500

    Check values of arrays after compression

--- src/test/java/com/example/ManualTests.java
+++ src/test/java/com/example/ManualTests.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
@@ -12,23 +13,27 @@ public class ManualTests {
     public void emptyArray() {
         char[] empty = {};
         assertEquals(0, stringManipulator.compress(empty));
+        assertArrayEquals(new char[]{}, empty);
     }
 
     @Test
     public void oneCharacter() {
         char[] oneChar = {'a'};
         assertEquals(1, stringManipulator.compress(oneChar));
+        assertArrayEquals(new char[]{'a'}, oneChar);
     }
 
     @Test
     public void oneGroupTwoChars() {
         char[] twoChars = {'a', 'a'};
         assertEquals(2, stringManipulator.compress(twoChars));
+        assertArrayEquals(new char[]{'a', '2'}, twoChars);
     }
 
     @Test
     public void twoCharGroups() {
         char[] twoGroups = {'a', 'a', 'b'};
         assertEquals(3, stringManipulator.compress(twoGroups));
+        assertArrayEquals(new char[]{'a', '2', 'b'}, twoGroups);
     }
 }
